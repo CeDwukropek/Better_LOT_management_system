6faddff18378db26a411ecfa0c5fb44c4153b832
Emil Kopytek <emilkopytek@gmail.com>
2023-10-04 12:33:46 +0200

    created database connection

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import javax.xml.transform.Result;
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/pracownicy";
+        String username = "root";
+        String password = "";
 
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection conn = DriverManager.getConnection(url, username, password);
+
+            Statement stm = conn.createStatement();
+
+            ResultSet res = stm.executeQuery("select * from pracownicy");
+            ResultSetMetaData rsmd = res.getMetaData();
+
+            while (res.next()) {
+                int x = rsmd.getColumnCount();
+                for (int i = 1; i <= x; i++) {
+                    System.out.println(res.getString(i));
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("DB error");
+        }
     }
 }
