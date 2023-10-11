b20b316996240fb241e032b843502c1a1de72a3d
Emil Kopytek <emilkopytek@gmail.com>
2023-10-11 11:46:28 +0200

    Working Customers system
    exported customers creation to external class

--- src/userCreator.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class userCreator {
-    private final DBConnection db;
-    private ArrayList<User> users = new ArrayList<>();
-    public userCreator(DBConnection db) {
-        this.db = db;
-    }
-}
