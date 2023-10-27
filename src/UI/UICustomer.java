99dd02b96bff34f25543de940434ca4f994bac95
Chmielarz <kamilchmiel24@gmail.com>
2023-10-27 11:54:03 +0200

    sdsdsdsdsd

--- src/UI/UICustomer.java
+++ src/UI/UICustomer.java
@@ -4,7 +4,14 @@ import javax.swing.*;
 
 public class UICustomer extends JFrame {
     public JPanel mainPanel;
-    private JLabel numertel;
+    private JLabel nameLabel;
+    private JLabel SurnameLabel;
+    private JLabel PhoneLabel;
+    private JLabel arriveLabel;
+    private JLabel DepartureLabel;
+    private JPanel NrFlightLabel;
+    private JLabel GateLabel;
+    private JLabel SeatLabel;
     public JLabel label;
 
     public UICustomer(String name) {
